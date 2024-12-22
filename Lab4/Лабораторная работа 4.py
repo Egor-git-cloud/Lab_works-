dca67856236af01c7fc5041e27e9c7915fcb4baa
Egor-git-cloud <egor20061312345@gmail.com>
2024-12-23 00:38:26 +0300

    Add files via upload

---
+++ Lab4/Лабораторная работа 4.py
@@ -0,0 +1,20 @@
+# #Задача 1
+# from math import sqrt
+# import datetime
+#
+# print(sqrt(25))
+# print(datetime.datetime.now())
+
+
+## Задача 2
+# import my_say_module as my
+# my.say('привет')
+
+## Задача 3
+from package import numbers as num
+# import package.numbers as num
+from package import strings as st
+num.plus(1,2)
+num.minus(9,8)
+
+st.megasplit('У Лукоморья дуб зелёный')
